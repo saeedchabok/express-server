48b22b15b0d49279d176d2d888b9d705019f84a7
Saeed Chabok <Saeed.chabok.1994@gmail.com>
2022-02-11 12:31:21 +0100

    Add envtemolate & routes

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 const express = require('express');
 const morgan = require('morgan');
+require('dotenv').config;
+
+const app = express();
+
+const port = process.env.PORT || 4000;
+
+app.use(express.json());
+app.use(morgan('tiny'));
+
+// app.use('/', home);
+// app.use('/listproducts', listproducts);
+// app.use('/users', users);
+// app.use('/orders', orders);
+// app.use('/contact', contact);
+
+app.listen(port, (err) => 
+{
+    if(err)
+        console.log(err);
+    else
+        console.log('app run in port ', port);
+
+});
