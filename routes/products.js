e9c1d48520b528ee66339871dc4bca685fcddd97
Saeed Chabok <Saeed.chabok.1994@gmail.com>
2022-02-13 22:54:03 +0100

    Add lowdb in get & post and conected with db.json

--- routes/products.js
+++ routes/products.js
@@ -1,27 +1,26 @@
 const express = require('express');
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
 
+const adapter = new FileSync('db.json');
+const db = low(adapter).get('products');
 const router = express.Router();
 
-const products =
-[
-    {
-        id: 1,
-        code:12345,
-        productsName: 'Reis'
-    }
-];
-
 router.route('/')
-    .get((req, res) => {res.status(200).send(products); })
+    .get((req, res) => 
+    {
+        const products = db;
+        res.status(200).send(products); 
+    })
     .post((req, res) => 
     {
         const product = 
         {
-            id: products[ products.length -1 ].id + 1,
+            id: db.__wrapped__.products[ db.__wrapped__.products.length -1 ].id + 1,
             code: req.body.code,
             productsName: req.body.productsName
         };
-        products.push(product);
+        db.push(product).write();
         res.status(200).send(product); 
     });
 module.exports = router;
